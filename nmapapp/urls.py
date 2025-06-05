0496d31091fe0c079f902b222a11789af3cd9531
Assad Akhoun <assad.akgoun@gmail.com>
2025-06-05 17:43:54 +0400

    Modification des fichiers views.py et urls.py (et autres) de l'application pour pouvoir mettre le résultat des scans sous format json

--- nmapapp/urls.py
+++ nmapapp/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('nmapscan/', views.nmap_scan.as_view(), name='nmap_scan'),
-    path('whatwebscan/', views.whatweb_scan.as_view(), name="whatweb_scan"),
-    path('zapscan/', views.zap_scan.as_view(), name="zap_scan"),
+    path('testjsonnmap/', views.getDataNmap),
+    path('testjsonzap/', views.getDataZap),
+    path('testjsonwhatweb/', views.getDataWhatweb),
+    # path('nmapscan/', views.nmap_scan.as_view(), name='nmap_scan'),
+    # path('whatwebscan/', views.whatweb_scan.as_view(), name="whatweb_scan"),
+    # path('zapscan/', views.zap_scan.as_view(), name="zap_scan"),
 ]
--- nmapapp/views.py
+++ nmapapp/views.py
@@ -1,55 +1,114 @@
-from django.views import View
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 import subprocess
 import time
 
-# Create your views here.
+@api_view(['GET'])
+def getDataNmap(request):
+    ip = "192.168.1.1"
+    commande = subprocess.run(["nmap", ip], capture_output=True, text=True)
+    resultat = commande.stdout.splitlines()
+    return Response({"ip" : ip, "result" : resultat})
 
-# Création de 3 classes pour pouvoir afficher le résultat des commandes PowerShell 
-class nmap_scan(View):
-    def get(self, request):
-        # Initialisation de l'IP
-        ip = "192.168.1.1"
-        # Commande stockée dans une variable et effectuée
-        commande = subprocess.run(["nmap", ip], capture_output=True, text=True)
-        # Résultat de la commande stocké
-        resultat = commande.stdout 
 
-        # Retour de l'affichage
-        return render(request, "nmapapp/nmap_scan.html", {"ip": ip, "resultat": resultat})
-    
-
-class whatweb_scan(View):
-    def get(self, request):
-        # Initialisation de l'URL
-        url = "http://testphp.vulnweb.com"
-        # Commande stockée dans une variable et effectuée
-        commande = subprocess.run(["wsl", "-d", "Ubuntu", "whatweb", url], capture_output=True, text=True)
-        # Résultat de la commande stocké
-        resultat = commande.stdout 
-
-        # Retour de l'affichage
-        return render(request, "nmapapp/whatweb_scan.html", {"url": url, "resultat": resultat})
-    
-
-class zap_scan(View):
-    def get(self, request):
-        # Initialisation de l'URL et de la clé alphanumérique
-        url = "http://testphp.vulnweb.com"
-        apikey = "80mb2scd3nqge4vnbu7midf1q1"
+@api_view(['GET'])
+def getDataZap(request):
+    url = "http://testphp.vulnweb.com"
+    apikey = "80mb2scd3nqge4vnbu7midf1q1"
         
-        # Lancement d'un scan Spider
-        spider_url = f"http://127.0.0.1:8090/JSON/spider/action/scan/?apikey={apikey}&url={url}"
-        ps_spider = f'Invoke-WebRequest -Uri "{spider_url}" -UseBasicParsing'
-        subprocess.run(["powershell", "-Command", ps_spider], capture_output=True, text=True)
+    # Lancement d'un scan Spider
+    spider_url = f"http://127.0.0.1:8090/JSON/spider/action/scan/?apikey={apikey}&url={url}"
+    ps_spider = f'Invoke-WebRequest -Uri "{spider_url}" -UseBasicParsing'
+    subprocess.run(["powershell", "-Command", ps_spider], capture_output=True, text=True)
 
-        # On attend quelques secondes que le Spider découvre les pages
-        time.sleep(5)
+    # On attend quelques secondes que le Spider découvre les pages
+    time.sleep(5)
 
-        # Lancer un scan actif (ascan)
-        ascan_url = f"http://127.0.0.1:8090/JSON/ascan/action/scan/?apikey={apikey}&url={url}"
-        ps_ascan = f'Invoke-WebRequest -Uri "{ascan_url}" -UseBasicParsing'
-        resultat = subprocess.run(["powershell", "-Command", ps_ascan], capture_output=True, text=True)
+    # Lancer un scan actif (ascan)
+    ascan_url = f"http://127.0.0.1:8090/JSON/ascan/action/scan/?apikey={apikey}&url={url}"
+    ps_ascan = f'Invoke-WebRequest -Uri "{ascan_url}" -UseBasicParsing'
+    commande = subprocess.run(["powershell", "-Command", ps_ascan], capture_output=True, text=True)
+    resultat = commande.stdout.splitlines()
 
-        # Retour de l'affichage
-        return render(request, "nmapapp/zap_scan.html", {"url": url,"resultat": resultat.stdout})
+    return Response({"url" : url, "result" : resultat})
+
+
+@api_view(['GET'])
+def getDataWhatweb(request):
+    url = "http://testphp.vulnweb.com"
+    # Commande stockée dans une variable et effectuée
+    commande = subprocess.run(["wsl", "-d", "Ubuntu", "whatweb", url], capture_output=True, text=True)
+    # Résultat de la commande stocké
+    resultat = commande.stdout.splitlines()
+
+    # Retour de l'affichage
+    return Response({"url" : url, "result" : resultat})
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.views import View
+# from django.shortcuts import render
+# import subprocess
+# import time
+# from rest_framework.response import Response
+
+# # Create your views here.
+
+# # Création de 3 classes pour pouvoir afficher le résultat des commandes PowerShell 
+# class nmap_scan(View):
+#     def get(self, request):
+#         # Initialisation de l'IP
+#         ip = "192.168.1.1"
+#         # Commande stockée dans une variable et effectuée
+#         commande = subprocess.run(["nmap", ip], capture_output=True, text=True)
+#         # Résultat de la commande stocké
+#         resultat = commande.stdout 
+
+#         # Retour de l'affichage
+#         return render(request, "nmapapp/nmap_scan.html", {"ip": ip, "resultat": resultat})
+    
+
+# class whatweb_scan(View):
+#     def get(self, request):
+#         # Initialisation de l'URL
+#         url = "http://testphp.vulnweb.com"
+#         # Commande stockée dans une variable et effectuée
+#         commande = subprocess.run(["wsl", "-d", "Ubuntu", "whatweb", url], capture_output=True, text=True)
+#         # Résultat de la commande stocké
+#         resultat = commande.stdout 
+
+#         # Retour de l'affichage
+#         return render(request, "nmapapp/whatweb_scan.html", {"url": url, "resultat": resultat})
+    
+
+# class zap_scan(View):
+#     def get(self, request):
+#         # Initialisation de l'URL et de la clé alphanumérique
+#         url = "http://testphp.vulnweb.com"
+#         apikey = "80mb2scd3nqge4vnbu7midf1q1"
+        
+#         # Lancement d'un scan Spider
+#         spider_url = f"http://127.0.0.1:8090/JSON/spider/action/scan/?apikey={apikey}&url={url}"
+#         ps_spider = f'Invoke-WebRequest -Uri "{spider_url}" -UseBasicParsing'
+#         subprocess.run(["powershell", "-Command", ps_spider], capture_output=True, text=True)
+
+#         # On attend quelques secondes que le Spider découvre les pages
+#         time.sleep(5)
+
+#         # Lancer un scan actif (ascan)
+#         ascan_url = f"http://127.0.0.1:8090/JSON/ascan/action/scan/?apikey={apikey}&url={url}"
+#         ps_ascan = f'Invoke-WebRequest -Uri "{ascan_url}" -UseBasicParsing'
+#         resultat = subprocess.run(["powershell", "-Command", ps_ascan], capture_output=True, text=True)
+
+#         # Retour de l'affichage
+#         return render(request, "nmapapp/zap_scan.html", {"url": url,"resultat": resultat.stdout})
