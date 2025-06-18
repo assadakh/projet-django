ea1a2fdd7f62e528157d2000e00b36be231cbd0b
Assad Akhoun <assad.akgoun@gmail.com>
2025-06-18 15:06:36 +0400

    Ajout d'un système de création d'utilisateur et d'authentification grâce à des tokens JWT

--- nmapapp/urls.py
+++ nmapapp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views 
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
 
 
 urlpatterns = [
+    # 🔐 Authentification JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Connexion (login)
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh token
+    path('api/register/', views.RegisterAPIView.as_view(), name='register'),  # Inscription (signup)
+
+    # 🔎 Vues de scans
     path('nmapscan/', views.NmapScanAPIView.as_view(), name='nmap-scan'),
     path('whatwebscan/', views.WhatWebScanAPIView.as_view(), name='whatweb-scan'),
     path('zapscan/', views.ZapScanAPIView.as_view(), name='zap-scan'),
--- nmapapp/views.py
+++ nmapapp/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
 from .models import ScanNmap, ScanWhatweb, ScanZap
 from .serializers import NmapScanSerializer, WhatWebResultSerializer, ZAPResultSerializer
 from .parsers import parse_nmap_output, parse_whatweb_output, parse_json_output
 
+from django.contrib.auth.models import User
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 import subprocess
@@ -12,16 +16,34 @@ import ipaddress
 import requests
 
 
+### Enregistrement d'utilisateur
+class RegisterAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response({"error": "Le nom d'utilisateur et le mot de passe sont obligatoires."}, status=400)
+
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "Nom d'utilisateur déjà utilisé."}, status=400)
+
+        User.objects.create_user(username=username, password=password)
+        return Response({"message": "Utilisateur créé avec succès."}, status=201)
 
 
 
 ### NMAPVIEW
 class NmapScanAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         ip = request.GET.get("ip")  
 
         if not ip:
-            return render(request, "nmap_scan.html", {
+            return Response({
                 "error": "Aucune IP fournie.",
                 "scans": [],
                 "url": ""
@@ -30,7 +52,7 @@ class NmapScanAPIView(APIView):
         try:
             ipaddress.ip_address(ip)
         except ValueError:
-            return render(request, "nmap_scan.html", {
+            return Response({
                 "error": "Adresse IP invalide. Veuillez entrer une IP correcte (ex : 192.168.1.1).",
                 "scans": [],
                 "ip": ip
@@ -39,14 +61,14 @@ class NmapScanAPIView(APIView):
         scan_exist = ScanNmap.objects.filter(ip=ip)
         if scan_exist.exists():
             serializer = NmapScanSerializer(scan_exist, many=True)
-            return render(request, "nmap_scan.html", {"ip": ip, "scans": serializer.data})
+            return Response({"ip": ip, "scans": serializer.data})
 
         ScanNmap.objects.filter(ip=ip).delete()
 
         commande = subprocess.run(["nmap", ip], capture_output=True, text=True)
 
         if commande.returncode != 0:
-            return render(request, "nmap_scan.html", {
+            return Response({
                 "error": "Le scan Nmap a échoué. Vérifiez que Nmap est installé et que l’IP est accessible.",
                 "scans": [],
                 "ip": ip
@@ -56,7 +78,7 @@ class NmapScanAPIView(APIView):
         resultat = parse_nmap_output(brut_output)
 
         if not resultat:
-            return render(request, "nmap_scan.html", {
+            return Response({
                 "error": "Aucun port détecté ou tous les ports sont filtrés.",
                 "scans": [],
                 "ip": ip
@@ -74,18 +96,24 @@ class NmapScanAPIView(APIView):
         ScanNmap.objects.bulk_create(scan_objects)
 
         scans = ScanNmap.objects.filter(ip=ip)
-        return render(request, "nmap_scan.html", {"ip": ip, "scans": scans})
+        serializer = NmapScanSerializer(scans, many=True)
+        return Response({
+            "ip": ip,
+            "scans": serializer.data
+        })
 
 
 
 
 ### WHATWEBVIEW
 class WhatWebScanAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         url = request.GET.get("url")
 
         if not url:
-            return render(request, "whatweb_scan.html", {
+            return Response({
                 "error": "Aucune URL fournie.",
                 "scans": [],
                 "url": ""
@@ -96,7 +124,7 @@ class WhatWebScanAPIView(APIView):
         try:
             validate(url)
         except ValidationError:
-            return render(request, "whatweb_scan.html", {
+            return Response({
                 "error": "Format d'URL invalide.",
                 "scans": [],
                 "url": url
@@ -107,7 +135,7 @@ class WhatWebScanAPIView(APIView):
             response = requests.get(url, timeout=5)
             response.raise_for_status()
         except requests.RequestException:
-            return render(request, "whatweb_scan.html", {
+            return Response({
                 "error": "L'URL n'est pas accessible (erreur réseau ou code HTTP non valide).",
                 "scans": [],
                 "url": url
@@ -119,7 +147,7 @@ class WhatWebScanAPIView(APIView):
             # Si oui, on récupère directement les données existantes
             serializer = WhatWebResultSerializer(scan_exist)
             result_list = serializer.data["result"].split(", ")
-            return render(request, "whatweb_scan.html", {
+            return Response({
                 "url": url,
                 "scans": result_list
             })
@@ -140,7 +168,7 @@ class WhatWebScanAPIView(APIView):
         serializer = WhatWebResultSerializer(scan_whatweb)
         result_list = serializer.data["result"].split(", ")
 
-        return render(request, "whatweb_scan.html", {
+        return Response({
             "url": url,
             "scans": result_list
         })
@@ -150,12 +178,14 @@ class WhatWebScanAPIView(APIView):
 
 ### ZAPVIEW
 class ZapScanAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         apikey = "80mb2scd3nqge4vnbu7midf1q1"
         url = request.GET.get("url")
 
         if not url:
-            return render(request, "zap_scan.html", {
+            return Response({
                 "error": "Aucune URL fournie.",
                 "scans": [],
                 "url": ""
@@ -166,7 +196,7 @@ class ZapScanAPIView(APIView):
         try:
             validate(url)
         except ValidationError:
-            return render(request, "zap_scan.html", {
+            return Response({
                 "error": "Format d'URL invalide.",
                 "scans": [],
                 "url": url
@@ -177,7 +207,7 @@ class ZapScanAPIView(APIView):
             response = requests.get(url, timeout=5)
             response.raise_for_status()
         except requests.RequestException:
-            return render(request, "zap_scan.html", {
+            return Response({
                 "error": "L'URL n'est pas accessible (erreur réseau ou code HTTP non valide).",
                 "scans": [],
                 "url": url
@@ -188,7 +218,7 @@ class ZapScanAPIView(APIView):
         if scan_exist.exists():
             # Si oui, on récupère directement les données existantes
             serializer = ZAPResultSerializer(scan_exist, many=True)
-            return render(request, "zap_scan.html", {"url": url, "scans": serializer.data})
+            return Response({"url": url, "scans": serializer.data})
 
         subprocess.run(
             ["powershell", "-Command",
@@ -229,102 +259,7 @@ class ZapScanAPIView(APIView):
 
         scans = ScanZap.objects.filter(url=url)
         serializer = ZAPResultSerializer(scans, many=True)
-        return render(request, "zap_scan.html", {"url": url, "scans": serializer.data})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class nmapscan(APIView):
-#     def get(self, request):
-#         ip = "192.168.1.1"
-
-#         # Lance la commande Nmap sur l'IP
-#         commande = subprocess.run(["nmap", ip], capture_output=True, text=True)
-
-#         # Nettoie la sortie : enlève les lignes vides
-#         brut_output = [line for line in commande.stdout.splitlines() if line.strip()]
-
-#         # Analyse la sortie brute avec le parseur pour récupérer les ports ouverts et services
-#         resultat = parse_nmap_output(brut_output)
-
-#         # Prépare les données structurées à renvoyer
-#         data = {"ip": ip, "result": resultat}
-
-#         # Sérialise et valide les données
-#         serializer = NmapScanSerializer(data=data)
-#         serializer.is_valid(raise_exception=True)
-#         return Response(serializer.data)
-
-
-
-# class whatwebscan(APIView):
-#     def get(self, request):
-#         url = "http://testphp.vulnweb.com"
-
-#         # Lance la commande WhatWeb via WSL sur l'URL cible
-#         commande = subprocess.run(["wsl", "-d", "Ubuntu", "whatweb", "--color=never", url], capture_output=True, text=True)
-
-#         # Nettoie la sortie brute pour enlever les lignes vides
-#         brut_output = [line for line in commande.stdout.splitlines() if line.strip()]
-
-#         # Analyse la sortie brute pour extraire la liste des plugins/technologies détectées
-#         resultat = parse_whatweb_output(brut_output)
-
-#         # Prépare les données à envoyer
-#         data = {"url": url, "result": resultat}
-
-#         # Sérialise et valide les données
-#         serializer = WhatWebResultSerializer(data=data)
-#         serializer.is_valid(raise_exception=True)
-#         return Response(serializer.data)
-
-
-
-# class zapscan(APIView):
-#     def get(self, request):
-#         url = "http://testphp.vulnweb.com"
-#         apikey = "80mb2scd3nqge4vnbu7midf1q1"
-
-#         # Lance le spider ZAP (scan d'exploration)
-#         spider_url = f"http://127.0.0.1:8090/JSON/spider/action/scan/?apikey={apikey}&url={url}"
-#         subprocess.run(["powershell", "-Command", f'Invoke-WebRequest -Uri "{spider_url}" -UseBasicParsing'],
-#                        capture_output=True, text=True)
-
-#         # Attend que le spider termine 
-#         time.sleep(5)
-
-#         # Lance le scan actif ZAP
-#         ascan_url = f"http://127.0.0.1:8090/JSON/ascan/action/scan/?apikey={apikey}&url={url}"
-#         subprocess.run(["powershell", "-Command", f'Invoke-WebRequest -Uri "{ascan_url}" -UseBasicParsing'],
-#                        capture_output=True, text=True)
-
-#         # Attend que le scan actif termine 
-#         time.sleep(15)
-
-#         # Récupère les alertes au format JSON via l’API ZAP
-#         alerts_url = f"http://127.0.0.1:8090/JSON/core/view/alerts/?baseurl={url}&apikey={apikey}"
-
-#         result = subprocess.run(["powershell", "-Command", f'(Invoke-WebRequest -Uri "{alerts_url}" -UseBasicParsing).Content'], capture_output=True, text=True)
-
-#         # Parse la sortie JSON
-#         alerts = parse_json_output(result.stdout)
-
-#         # Prépare la réponse
-#         data = {"url": url, "result": alerts}
-
-#         # Sérialise et retourne la réponse
-#         serializer = ZAPResultSerializer(data=data)
-#         serializer.is_valid(raise_exception=True)
-#         return Response(serializer.data)
+        return Response({
+            "url": url,
+            "scans": serializer.data
+        })
